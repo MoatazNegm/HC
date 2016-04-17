64cb34279625d562f2aaedd7d008d0f10b0c9b15
Moataz Negm <sales@quickstor.net>
2016-04-17 14:28:33 -0400

    making automated adding new pools

--- iscsirefresh.sh
+++ iscsirefresh.sh
@@ -5,6 +5,8 @@ while read -r  hostline ; do
  host=`echo $hostline | awk '{print $1}'`
  ping -c 1 -W 1 $host &>/dev/null
  if [ $? -eq 0 ]; then
+  hostpath=`ls /var/lib/iscsi/nodes | grep "$host"`;
+  rm -rf /var/lib/iscsi/nodes/$hostpath &>/dev/null
   hostiqn=`/sbin/iscsiadm -m discovery --portal $host --type sendtargets | awk '{print $2}'`
   /sbin/iscsiadm -m node --targetname $hostiqn --portal $host -u
   /sbin/iscsiadm -m node --targetname $hostiqn --portal $host -l
