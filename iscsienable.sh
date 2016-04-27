e34ae83ef25c68188624e7b23a1691d196495777
Moataz Negm <sales@quickstor.net>
2016-04-27 14:57:23 -0400

    fixing iscsi start

---
+++ iscsienable.sh
@@ -0,0 +1,9 @@
+cd /pace
+iscsimapping='/pace/iscsimapping';
+iscsitargets='/pace/iscsitargets';
+while read -r  hostline ; do
+ host=`echo $hostline | awk '{print $2}'`
+ rm -rf /var/lib/iscsi/nodes/iqn.2016-03.com.${host}:t1 
+ rm -rf /var/lib/iscsi/nodes/${host}* &>/dev/null
+done < $iscsitargets
+systemctl start iscsi
