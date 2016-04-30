9035f6ff3fe9fdfc5eff750dc64f42abe9c98370
Moataz Negm <sales@quickstor.net>
2016-04-30 13:14:10 -0400

    fixing ZFSstart

--- ZFSstart.sh
+++ ZFSstart.sh
@@ -3,18 +3,27 @@ iscsimapping='/pacedata/iscsimapping'
 declare -a pools=();
 /sbin/zpool export -a
 sh iscsienable.sh
-sh addtargetdisks.sh
-sh iscsirefresh.sh
+sh addtargetdisks.sh &>/dev/null
+sh iscsirefresh.sh &>/dev/null
 sleep 2;
 sh listingtargets.sh 
 runninghosts=`cat $iscsimapping | grep -v notconnected | wc -l`
 if [ $runninghosts -le 1 ]; then
+ echo searching pools
  pools=(`/sbin/zpool import | grep "pool:" | awk '{print $2}'`);
- for pool in "${pools[@]}"; do
-  ls /pacedata/pools/ | grep $pool &>/dev/null
-  if [ $? -eq 0 ]; then
-   /sbin/zpool import -c '/pacedata/pools/'${pool}.cache $pool;
-   /sbin/zpool clean $pool;
-  fi
- done
+ if [ ${#pools[@]} -gt 0 ]; then
+  echo found pools ${pools[@]}
+  for pool in "${pools[@]}"; do
+    echo importing $pool
+   ls /pacedata/pools/ | grep $pool &>/dev/null
+   if [ $? -eq 0 ]; then
+    echo start import
+    /sbin/zpool import -c '/pacedata/pools/'${pool}.cache $pool;
+   else
+    echo as it is $pool
+    /sbin/zpool import $pool;
+   fi
+   /sbin/zpool clear $pool;
+  done
+ fi
 fi
