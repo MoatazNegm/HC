221a7c09c438e1420c09c0bfd9466ec3a83d246a
dskfjlk@dlfjs.ckldj <dskfjlk@dlfjs.ckldj>
2018-06-30 14:49:49 +0200

    fixing

--- poolsANDraids.py
+++ poolsANDraids.py
@@ -6,3 +6,25 @@ raidtypes=['mirror','raidz','stripe']
 raid2=['log','cache','spare']
 zpool=[]
 for a in y:
+ b=a.split()
+ if "pdhc" in a and  'pool' not in a:
+  raidlist=[]
+  zdict={}
+  zdict={ 'name':b[0], 'status':b[1], 'raidlist': raidlist }
+  zpool.append(zdict)
+ elif any(raid in a for raid in raidtypes):
+  disklist=[]
+  rdict={ 'name':b[0], 'status':b[1],'disklist':disklist }
+  raidlist.append(rdict)
+ elif any(raid in a for raid in raidtypes):
+  disklist=[]
+  zdict={ 'name':b[0], 'status':'NA','disklist':disklist }
+  raidlist.append(rdict)
+ elif 'scsi' in a:
+   zdict={'name':b[0], 'status':b[1]}
+   disklist.append(zdict)
+ else:
+   zdict={'name':'na','status':a}
+   #zpool.append(zdict)
+   
+print(zpool)
