fc73549751f71bc3ebb8b4ffcf5dca9eb9bda13b
sdlfkj <dkdjkl@dljkf.com>
2025-07-22 15:07:38 +0300

    fixing

--- selectimportlooper.sh
+++ selectimportlooper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
-leaderip=echo $@ | awk '{print $1}'
-myhost=echo $@ | awk '{print $2}'
+leaderip=`echo $@ | awk '{print $1}'`
+myhost=`echo $@ | awk '{print $2}'`
 selectimport() {
 cd /pace
 /pace/selectimport.py $leaderip $myhost 1>/root/selectimport.log 2>/root/selectimporterr.log 
